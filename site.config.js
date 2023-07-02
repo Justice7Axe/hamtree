fe96da8203ac5e9848ebf1317ae034467b2e6e54
Justice7Axe <109892581+Justice7Axe@users.noreply.github.com>
2023-07-03 00:11:27 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -53,7 +53,7 @@ const CONFIG = {
   utterances: {
     enable: true,
     config: {
-      repo: "morethanmin/morethan-log",
+      repo: "Justice7Axe/hamtree",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
